f3b74f5cced1993c2bdae966dba16e7744465dcb
Tom Fogal <tfogal@alumni.unh.edu>
2014-12-29 23:54:07 +0100

    add some section header tests.

--- tests/simple.rs
+++ tests/simple.rs
@@ -7,6 +7,7 @@ fn testexectype() {
 		Ok(hdr) => hdr,
 	};
 	assert_eq!(bintrue.file_type, elf::FileType::Executable);
+	assert_eq!(bintrue.n_shdr, 28); // is this stable across systems?
 
 	let libc = match elf::ehdr("/lib/x86_64-linux-gnu/libc.so.6") {
 		Err(err) => panic!("error getting libc's header: {}", err),
@@ -16,6 +17,35 @@ fn testexectype() {
 
 	printinfo("/lib/x86_64-linux-gnu/libc.so.6");
 	printinfo("/bin/true");
+}
+
+#[test]
+fn test_sec_headers() {
+	let bintrue = match elf::ehdr("/bin/true") {
+		Err(err) => panic!("error getting hdr for /bin/true: {}", err),
+		Ok(hdr) => hdr,
+	};
+	for i in range(0u, bintrue.n_shdr as uint) {
+		let sh = match elf::shdr("/bin/true", i) {
+			Err(err) => panic!("no header {}: {}", i, err),
+			Ok(header) => header,
+		};
+		// load address on x86-64 is 0x400000, so all section headers should at
+		// least be after that.  but 0x0 is a special value, so that's okay.
+		assert!(sh.addr == 0x0 || sh.addr > 0x400000);
+	}
+}
+
+#[test]
+fn test_invalid_sec_header() {
+	let bintrue = match elf::ehdr("/bin/true") {
+		Err(err) => panic!("error getting hdr for /bin/true: {}", err),
+		Ok(hdr) => hdr,
+	};
+	match elf::shdr("/bin/true", bintrue.n_shdr as uint) {
+		Err(_) => return,
+		Ok(_) => panic!("'succeeded' reading a nonexistent section header"),
+	}
 }
 
 #[allow(dead_code)] // stupid f'ing rust doesn't consider test code as "used".
